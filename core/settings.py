c64cc0453b6c34429d86af4d83ea9507746a8963
test <test@MacBook-Pro-test.local>
2026-06-09 02:46:26 +0300

    Force fix: add comma

--- core/settings.py
+++ core/settings.py
@@ -32,16 +32,16 @@ CSRF_TRUSTED_ORIGINS = [
     'https://brew-salvation-production.up.railway.app',
     'https://*.railway.app',
 ]
+
 INSTALLED_APPS = [
     'catalog',
     'articles',
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.admin',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-     # 'django_extensions',  # ← ЗАКОММЕНТИРОВАНО
+    'django.contrib.staticfiles',    
 ]
 
 MIDDLEWARE = [
