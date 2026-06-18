6d3198a755ee0c2426e0e1d7b9a5eb7349c4cc72
test <test@MacBook-Pro-test.local>
2026-06-18 15:39:51 +0300

    aeddhdjehdj

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views  # <-- добавить
+from core import views as core_views                # <-- добавить
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', include('articles.urls')),
+    
     # ===== НОВЫЕ МАРШРУТЫ =====
     path('register/', core_views.register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
