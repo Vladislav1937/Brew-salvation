9317c40d7c98fd2cb42a011e3ae55873cb924084
test <test@MacBook-Pro-test.local>
2026-06-18 13:24:05 +0300

    i like coffee

--- articles/urls.py
+++ articles/urls.py
@@ -6,8 +6,7 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.article_list, name='article_list'),
     path('category/<slug:category_slug>/', views.article_list, name='category_filter'),
-     path('category/<slug:category_slug>/', views.article_list, name='article_list_by_category'),
     path('calculator/', views.brewing_calculator, name='brewing_calculator'),
-    path('like/<slug:slug>/', views.like_article, name='like_article'),# ← ПОДНЯЛИ ВВЕРХ
+    path('like/<slug:slug>/', views.like_article, name='like_article'),
     path('<slug:slug>/', views.article_detail, name='article_detail'),
 ]
